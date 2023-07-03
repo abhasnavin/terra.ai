bc8ca43056e5708ff891af08399b2d7325aee4ba
rg <rg6393@gmail.com>
2023-07-03 12:37:56 +0530

    testing additional fines

---
+++ env/test/s3.tf
@@ -0,0 +1,18 @@
+module "s3_bucket" {
+  source = "../../modules/s3"
+
+  enable_s3_bucket      = true
+  enable_s3_bucket_acl  = true
+  s3_bucket_name        = "dev_s3_bucket"
+  s3_bucket_prefix      = null
+  s3_bucket_force_destroy       = false
+  s3_bucket_object_lock_enabled = false
+  s3_bucket_acl_bucket  = ""
+  s3_bucket_acl_acl     = "private"
+  s3_bucket_acl_expected_bucket_owner = ""
+  s3_bucket_acl_access_control_policy = {}
+  tags = {
+    env  = "dev"
+    name = "dev_s3_bucket!"
+  }
+}
