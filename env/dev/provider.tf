bce22c583d3c0d5f6bea09665d6849aec4467c22
rg <rg6393@gmail.com>
2023-07-04 09:56:42 +0530

    updated provider config as per latest and deploy actions

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -1,9 +1,16 @@
 terraform {
-  required_version = ">= 1.4"  # Specify the minimum required Terraform version
+  required_version = ">= 1.4" # Specify the minimum required Terraform version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0, < 4.0"
+      region  = "us-west-2"
+    }
+  }
 }
-
-provider "aws" {
-  version = ">= 3.0, < 4.0"  # Specify the AWS provider version constraint
-  region  = "us-west-2"      # Update with your desired AWS region
-  # Add any other provider configurations if needed
-}
+#
+#provider "aws" {
+#  version = ">= 3.0, < 4.0"  # Specify the AWS provider version constraint
+#  region  = "us-west-2"      # Update with your desired AWS region
+#  # Add any other provider configurations if needed
+#}
