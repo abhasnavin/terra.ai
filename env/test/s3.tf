7996ed0fd7d44c4b5ad4ef0e5f92e95fa138a810
Rahul Goyal <rg6393@gmail.com>
2023-07-07 21:15:40 +0530

    deployment in dev and test

--- env/test/s3.tf
+++ env/test/s3.tf
@@ -1,18 +1,24 @@
-module "s3_bucket" {
-  source = "../../modules/s3"
+module "s3_private_bucket" {
+  source      = "../../modules/s3"
 
-  enable_s3_bucket                    = true
-  enable_s3_bucket_acl                = true
-  s3_bucket_name                      = "test-s3-bucket-rg01"
-  s3_bucket_prefix                    = null
-  s3_bucket_force_destroy             = false
-  s3_bucket_object_lock_enabled       = false
-  s3_bucket_acl_bucket                = ""
-  s3_bucket_acl_acl                   = "private"
-  s3_bucket_acl_expected_bucket_owner = ""
-  s3_bucket_acl_access_control_policy = {}
+  # AWS S3 bucket
+  enable_s3_bucket = true
+  s3_bucket_name   = "test-s3-bucket-rg01"
+
+  // Enable S3 bucket ACL
+  enable_s3_bucket_acl = true
+  s3_bucket_acl_acl    = "private"
+
+  //enable s3 bucket ownership
+  enable_s3_bucket_ownership_controls = true
+  s3_bucket_ownership_controls_rule   = [
+    {
+      object_ownership = "ObjectWriter"
+    }
+  ]
+
   tags = {
-    env  = "test1"
-    type = "storage"
+    "Environment"   = "test",
+    "Orchestration" = "Terraform"
   }
 }
