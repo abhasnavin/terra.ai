a0c02a2c23abb8c46c0798314fe5a88b159444bb
Rahul Goyal <rg6393@gmail.com>
2023-07-07 14:38:35 +0530

    updated backend

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -6,14 +6,19 @@ terraform {
       version = ">= 3.0, < 4.0"
     }
   }
-  #  backend "remote" {
-  #    hostname     = "app.terraform.io"
-  #    organization = "terraAI"
-  #    workspaces { name = "ai-dev" }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "terraAI"
+    workspaces { name = "ai-dev" }
+  }
+  #  backend "s3" {
+  #    bucket = "glue-terraform-backend"
+  #    key    = "terraform.tfstate"
+  #    region = "ap-southeast-2"
   #  }
 }
-#
+
 provider "aws" {
-  region  = "us-west-2"      # Update with your desired AWS region
+  region = "us-west-2" # Update with your desired AWS region
   # Add any other provider configurations if needed
 }
