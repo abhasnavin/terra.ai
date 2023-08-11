37c9b7179a96efff8c8507f4417b2a3605aba43a
Rahul Goyal <rg6393@gmail.com>
2023-08-11 05:59:23 +0000

    ChatGPT response updated

--- env/dev/s3.tf
+++ env/dev/s3.tf
@@ -1,6 +1,6 @@
 #Chatbot says: 
 module "s3_bucket" {
-  source = "../../modules/s3"
+  source = "./modules/s3"
 
   enable_s3_bucket = true
   s3_bucket_name = "my-private-s3-bucket"
@@ -9,17 +9,16 @@ module "s3_bucket" {
   s3_bucket_object_lock_enabled = null
 
   enable_s3_bucket_acl = true
-  s3_bucket_acl_bucket = "my-private-s3-bucket"
+  s3_bucket_acl_bucket = ""
   s3_bucket_acl_expected_bucket_owner = null
   s3_bucket_acl_acl = "private"
   s3_bucket_acl_access_control_policy = []
 
   enable_s3_bucket_ownership_controls = true
-  s3_bucket_ownership_controls_bucket = "my-private-s3-bucket"
-  s3_bucket_ownership_controls_rule = []
-
-  tags = {
-    Name = "my-private-s3-bucket"
-    Environment = "PROD"
-  }
+  s3_bucket_ownership_controls_bucket = ""
+  s3_bucket_ownership_controls_rule = [
+    {
+      object_ownership = "ObjectWriter"
+    }
+  ]
 }
