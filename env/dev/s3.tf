2ffa51f867e09d3d7a6f31ed35534a8067796029
Rahul Goyal <rg6393@gmail.com>
2023-08-23 19:30:47 +0000

    ChatGPT response updated

--- env/dev/s3.tf
+++ env/dev/s3.tf
@@ -3,19 +3,19 @@ module "s3_bucket" {
   source = "../../modules/s3"
 
   enable_s3_bucket = true
-  s3_bucket_name = "uh-private-s3-bucket"
+  s3_bucket_name = "gt-private-s3-bucket"
   s3_bucket_force_destroy = null
   s3_bucket_prefix = null
   s3_bucket_object_lock_enabled = null
 
   enable_s3_bucket_acl = true
-  s3_bucket_acl_bucket = "uh-private-s3-bucket"
+  s3_bucket_acl_bucket = "gt-private-s3-bucket"
   s3_bucket_acl_expected_bucket_owner = null
   s3_bucket_acl_acl = "private"
   s3_bucket_acl_access_control_policy = []
 
   enable_s3_bucket_ownership_controls = true
-  s3_bucket_ownership_controls_bucket = "uh-private-s3-bucket"
+  s3_bucket_ownership_controls_bucket = "gt-private-s3-bucket"
   s3_bucket_ownership_controls_rule = [
     {
       object_ownership = "ObjectWriter"
@@ -23,7 +23,8 @@ module "s3_bucket" {
   ]
 
   tags = {
-    Name = "uh-private-s3-bucket"
+    Name = "gt-private-s3-bucket"
+    environment = "PROD"
     env = "dev"
   }
 }
