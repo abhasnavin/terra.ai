a93a9a34a8c538cf7d6709c15dc7c690a46624d9
Rahul Goyal <rg6393@gmail.com>
2023-09-05 19:09:56 +0000

    working now

---
+++ github_helpers.py
@@ -0,0 +1,72 @@
+from github import Github
+from github import Auth
+import time
+
+# Constants
+CHECK_INTERVAL = 60
+
+def get_workflow_status(repo_name, workflow_id, github_token):
+    auth = Auth.Token(github_token)
+    g = Github(auth=auth)
+    repo = g.get_user().get_repo(repo_name)
+    workflow = repo.get_workflow(workflow_id)
+    # print(workflow)
+    
+    max_checks = 30
+    status = None  # Initialize status before the loop
+    time.sleep(max_checks)
+
+    for _ in range(max_checks):
+        try:
+            runs = workflow.get_runs()
+            if runs.totalCount > 0:
+                latest_run = runs[0]
+                status = latest_run.conclusion                
+                
+            if status == "success":
+                return "success"
+            elif status == "failure":
+                return "failure"
+        except StopIteration:
+            break
+        except Exception as e:
+            print("Error:", e)
+            
+        time.sleep(CHECK_INTERVAL)
+
+    return status  # Return the final status after the loop
+
+def get_most_recently_pushed_branch(repo):
+    most_recent_commit = None
+    most_recent_branch = None
+    
+    for branch in repo.get_branches():
+        commit = branch.commit
+        if most_recent_commit is None or commit.commit.author.date > most_recent_commit.commit.author.date:
+            most_recent_commit = commit
+            most_recent_branch = branch
+            
+    return most_recent_branch
+
+def raise_pull_request(repo_name, github_token):
+    auth = Auth.Token(github_token)
+    g = Github(auth=auth)
+    repo = g.get_user().get_repo(repo_name)
+    
+    default_branch = repo.default_branch
+    
+    most_recent_branch = get_most_recently_pushed_branch(repo)
+    
+    if most_recent_branch:
+        head_branch_name = most_recent_branch.name
+        head = f"{repo.owner.login}:{head_branch_name}"  # Format it as "owner:branch"
+        title = "Feature Branch Pull Request"
+        body = f"This is a pull request from the {head_branch_name} branch."
+        
+        try:
+            pull_request = repo.create_pull(title=title, body=body, base=default_branch, head=head)
+            return f"Pull request created successfully! URL: {pull_request.html_url}"
+        except Exception as e:
+            return f"Error creating pull request: {str(e)}"
+    else:
+        return "No branches found."
---
+++ test_github_helpers.py
@@ -0,0 +1,23 @@
+from github_helpers import get_workflow_status, raise_pull_request
+from config import repo_name, github_token, workflow_id
+
+def test_get_workflow_status():
+    status = get_workflow_status(repo_name, workflow_id, github_token)
+    if status == "success":
+        print("Github Workflow successfully completed!")
+    elif status == "failure":
+        print("Workflow failed!")
+    else:
+        print("Unable to determine workflow status.")
+
+def test_raise_pull_request():
+    result = raise_pull_request(repo_name, github_token)
+    if result.startswith("Pull request created successfully! URL:"):
+        print("Pull request created successfully!")
+        print(result)  # Print the URL
+    else:
+        print("Failed to create a pull request.")
+
+if __name__ == "__main__":
+    #test_get_workflow_status()
+    test_raise_pull_request()
