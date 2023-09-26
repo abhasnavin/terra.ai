7ea1ce5cae22810ef8714cf881d0f1562c85bb14
Rahul Goyal <rg6393@gmail.com>
2023-09-26 08:23:24 +0000

    ChatGPT response updated

--- env/dev/s3.tf
+++ env/dev/s3.tf
@@ -3,14 +3,25 @@ module "s3_bucket" {
   source = "../../modules/s3"
 
   enable_s3_bucket = true
-  s3_bucket_name = "amogh-private-s3-bucket"
+  s3_bucket_name = "new-private-s3-bucket"
   s3_bucket_acl_acl = "private"
+  enable_s3_bucket_acl = true
+  s3_bucket_acl_access_control_policy = [
+    {
+      owner = {
+        id = "ObjectWriter"
+        display_name = "ObjectWriter"
+      }
+      grant = []
+    }
+  ]
   enable_s3_bucket_ownership_controls = true
   s3_bucket_ownership_controls_rule = [
     {
       object_ownership = "ObjectWriter"
     }
   ]
+
   tags = {
     env = "dev"
   }
