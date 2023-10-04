31a9dced1ffb92954df6237f333e8b952777843e
abhas navin <a.navin2710@gmail.com>
2023-10-04 07:33:19 +0000

    ChatGPT response updated

--- env/dev/s3.tf
+++ env/dev/s3.tf
@@ -3,25 +3,23 @@ module "s3_bucket" {
   source = "../../modules/s3"
 
   enable_s3_bucket = true
-  s3_bucket_name = "my-techconnect-privates3-bucket"
-  s3_bucket_force_destroy = null
-  s3_bucket_prefix = null
-  s3_bucket_object_lock_enabled = null
-
-  enable_s3_bucket_acl = true
-  s3_bucket_acl_bucket = "my-techconnect-privates3-bucket"
-  s3_bucket_acl_expected_bucket_owner = null
+  s3_bucket_name = "mytechconnectdemos3"
   s3_bucket_acl_acl = "private"
-  s3_bucket_acl_access_control_policy = []
-
+  enable_s3_bucket_acl = true
+  s3_bucket_acl_access_control_policy = [
+    {
+      owner = {
+        id = "ObjectWriter"
+      },
+      grant = []
+    }
+  ]
   enable_s3_bucket_ownership_controls = true
-  s3_bucket_ownership_controls_bucket = "my-techconnect-privates3-bucket"
   s3_bucket_ownership_controls_rule = [
     {
       object_ownership = "ObjectWriter"
     }
   ]
-
   tags = {
     env = "dev"
   }
